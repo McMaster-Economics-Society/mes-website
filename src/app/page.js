0893a377f472f61f5267a6c85f0d1556c774a955
Winsome Tang <tangwins@sheridancollege.ca>
2025-06-02 14:37:07 -0400

    chore: try configuring deployment issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to My Website</h1>
-      <p>This is my custom content!</p>
+      <h1>Welcome to McMaster&apos;s first Econ Society Club Website</h1>
+      <p>This is my custom content! HELLLOOOOO</p>
       {/* Add your actual website content here */}
     </div>
   );
